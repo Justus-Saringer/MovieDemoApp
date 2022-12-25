a2edd56aff32bf70eebe978722316fa3c9f60f06
Justus Saringer <justus.saringer@check24.de>
2022-12-25 23:23:00 +0100

    added persistation of user login data

--- app/src/main/java/de/saringer/moviedemoapp/features/login/network/LoginRepository.kt
+++ app/src/main/java/de/saringer/moviedemoapp/features/login/network/LoginRepository.kt
@@ -1,21 +1,71 @@
 package de.saringer.moviedemoapp.features.login.network
 
+import android.app.Application
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+private val Context.dataStore by preferencesDataStore(name = "user_login_data")
+
 class LoginRepository @Inject constructor(
-    private val api: LoginApi
+    private val api: LoginApi,
+    application: Application
 ) {
+    private val dataStore = application.applicationContext.dataStore
+
     suspend fun getRequestToken() = api.getRequestToken()
 
     suspend fun getSessionIdForGuests() = api.getSessionIdForGuests()
 
     suspend fun getSessionIdWithUserData(
-        username : String,
-        password : String,
+        username: String,
+        password: String,
         requestToken: String
     ) = api.getSessionIdWithUserData(
         username = username, password = password, requestToken = requestToken
     )
 
     suspend fun deleteSession() = api.deleteSession()
+
+    fun saveLoginData(username: String, password: String) {
+        CoroutineScope(Dispatchers.Default).launch {
+            dataStore.edit { prefs ->
+                prefs[LoginKeys.USERNAME] = username
+                prefs[LoginKeys.PASSWORD] = password
+            }
+        }
+    }
+
+    fun getStoredUsername(): String? {
+        var username: String? = null
+
+        CoroutineScope(Dispatchers.Main).launch {
+            dataStore.data.collect { preferences ->
+                username = preferences[LoginKeys.USERNAME]
+            }
+        }
+        return username
+    }
+
+    fun getStoredPassword(): String? {
+        var password: String? = null
+
+        CoroutineScope(Dispatchers.Main).launch {
+            dataStore.data.collect { preferences ->
+                password = preferences[LoginKeys.PASSWORD]
+            }
+        }
+        return password
+    }
+
+    private object LoginKeys {
+        val USERNAME = stringPreferencesKey("username")
+        val PASSWORD = stringPreferencesKey("password")
+    }
 }
+
