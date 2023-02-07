dbccc9a01cf3b4702ee345423b2fa7673793e033
Justus Saringer <justus.saringer@check24.de>
2023-02-07 23:50:57 +0100

    outsourced LinearLoadingIndicator

--- app/src/main/java/de/saringer/moviedemoapp/features/login/ui/LoginScreen.kt
+++ app/src/main/java/de/saringer/moviedemoapp/features/login/ui/LoginScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ehsanmsz.mszprogressindicator.progressindicator.LineScaleProgressIndicator
 import de.saringer.moviedemoapp.R
+import de.saringer.moviedemoapp.shared.composables.LinearLoadingIndicator
 import de.saringer.moviedemoapp.ui.theme.MovieDemoAppTheme
 import de.saringer.moviedemoapp.ui.theme.orange
 import de.saringer.moviedemoapp.ui.theme.yellow
@@ -86,16 +87,7 @@ private fun LoadingIndication(isVisible: Boolean) {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            LineScaleProgressIndicator(
-                color = yellow,
-                animationDuration = 800,
-                animationDelay = 200,
-                startDelay = 0,
-                lineWidth = 3.dp,
-                lineCount = 5,
-                minLineHeight = 3.dp,
-                maxLineHeight = 30.dp
-            )
+            LinearLoadingIndicator()
         }
     }
 }
