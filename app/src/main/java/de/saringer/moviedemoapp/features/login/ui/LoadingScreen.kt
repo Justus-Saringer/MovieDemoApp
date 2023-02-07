d22f0df19321d0746cd5baf4ba1565ec2fdbd067
Justus Saringer <justus.saringer@check24.de>
2023-02-07 23:49:51 +0100

    added LoadingScreen.kt

---
+++ app/src/main/java/de/saringer/moviedemoapp/features/login/ui/LoadingScreen.kt
@@ -0,0 +1,53 @@
+package de.saringer.moviedemoapp.features.login.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ehsanmsz.mszprogressindicator.progressindicator.LineScaleProgressIndicator
+import de.saringer.moviedemoapp.shared.composables.LinearLoadingIndicator
+import de.saringer.moviedemoapp.ui.theme.MovieDemoAppTheme
+import de.saringer.moviedemoapp.ui.theme.yellow
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Title()
+
+        Spacer(modifier = Modifier.height(8.dp))
+        // TODO: create shared and move this to it, several usages
+        LinearLoadingIndicator()
+    }
+}
+
+@Composable
+private fun Title() {
+    Text(
+        text = "TMDB App",
+        style = MaterialTheme.typography.h6,
+        color = MaterialTheme.colors.onBackground
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    MovieDemoAppTheme {
+        LoadingScreen()
+    }
+}
