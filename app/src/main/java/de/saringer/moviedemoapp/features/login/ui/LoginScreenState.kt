47a0f06739e638286d4ba627afdfa9898717d09f
Justus-Saringer <jsaringer@gmx.de>
2023-02-16 00:10:24 +0100

    added parameters and computed properties to LoginScreenState.kt

--- app/src/main/java/de/saringer/moviedemoapp/features/login/ui/LoginScreenState.kt
+++ app/src/main/java/de/saringer/moviedemoapp/features/login/ui/LoginScreenState.kt
@@ -1,5 +1,6 @@
 package de.saringer.moviedemoapp.features.login.ui
 
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.runtime.mutableStateOf
 
 class LoginScreenState constructor(
@@ -7,12 +8,25 @@ class LoginScreenState constructor(
     passwordInput: String,
     isLoading: Boolean,
     isPasswordVisible: Boolean,
-    hasError: Boolean = false,
 ) {
-
     val usernameInput = mutableStateOf(usernameInput)
     val passwordInput = mutableStateOf(passwordInput)
     val isLoading = mutableStateOf(isLoading)
     val isPasswordVisible = mutableStateOf(isPasswordVisible)
-    val hasError = mutableStateOf(hasError)
+    val snackBarHostState = SnackbarHostState()
+    val isLoginSuccessful = mutableStateOf(false)
+
+    private val areInputFieldsEmpty: Boolean
+        get() = usernameInput.value.isEmpty() || passwordInput.value.isEmpty()
+
+    val hasPageError: Boolean
+        get() = areInputFieldsEmpty ||
+                isLoginSuccessful.value
+
+    val errorText: String
+        get() = when {
+            usernameInput.value.isEmpty() -> "Username is empty"
+            passwordInput.value.isEmpty() -> "Password is empty"
+            else -> "Password or Username is incorrect"
+        }
 }
