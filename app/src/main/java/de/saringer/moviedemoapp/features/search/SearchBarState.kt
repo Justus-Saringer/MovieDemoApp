d7a49f30263ed7cefef668d63933a1339890fd50
Justus-Saringer <jsaringer@gmx.de>
2023-02-19 14:20:52 +0100

    added SearchBarState.kt

---
+++ app/src/main/java/de/saringer/moviedemoapp/features/search/SearchBarState.kt
@@ -0,0 +1,10 @@
+package de.saringer.moviedemoapp.features.search
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+data class SearchBarState(
+    val searchInput: MutableState<String> = mutableStateOf(""),
+    val selectedChip: MutableState<String> = mutableStateOf(""),
+    val chips: List<ChipState> = emptyList()
+)
