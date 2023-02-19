bd23b5c304a439465983bd555aee1b0b9ec61fd2
Justus-Saringer <jsaringer@gmx.de>
2023-02-19 14:20:41 +0100

    added SearchScreenState.kt

---
+++ app/src/main/java/de/saringer/moviedemoapp/features/search/SearchScreenState.kt
@@ -0,0 +1,15 @@
+package de.saringer.moviedemoapp.features.search
+
+import androidx.compose.runtime.mutableStateOf
+
+data class SearchScreenState(
+    val searchBarState: SearchBarState = SearchBarState(
+        chips = listOf(
+            ChipState(text = mutableStateOf("People")),
+            ChipState(text = mutableStateOf("Movies")),
+            ChipState(text = mutableStateOf("TV Shows")),
+            ChipState(text = mutableStateOf("Companies")),
+            ChipState(text = mutableStateOf("Multiple")),
+        )
+    )
+)
