2282053a6a1302e7bc848aae55864c88f39d454c
Justus-Saringer <jsaringer@gmx.de>
2023-02-20 20:49:13 +0100

    clean up

--- app/src/main/java/de/saringer/moviedemoapp/extensions/Extensions.kt
+++
@@ -1,2 +0,0 @@
-package de.saringer.moviedemoapp.extensions
-
--- app/src/main/java/de/saringer/moviedemoapp/ui/theme/Color.kt
+++ app/src/main/java/de/saringer/moviedemoapp/ui/theme/Color.kt
@@ -6,6 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val white = Color(0xFFFFFFFF)
 
 val nearlyBlack = Color(0xFF222224)
 val orange = Color(0xFFef7f38)
