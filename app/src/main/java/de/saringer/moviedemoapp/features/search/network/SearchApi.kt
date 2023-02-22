4ca4b2ad1cd3e36400ed59659ed7acf63746faaf
Justus-Saringer <jsaringer@gmx.de>
2023-02-23 00:04:26 +0100

    added search api routes

--- app/src/main/java/de/saringer/moviedemoapp/features/search/network/SearchApi.kt
+++ app/src/main/java/de/saringer/moviedemoapp/features/search/network/SearchApi.kt
@@ -1,4 +1,53 @@
 package de.saringer.moviedemoapp.features.search.network
 
+import de.saringer.moviedemoapp.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface SearchApi {
+    @GET("search/companies?api_key=${BuildConfig.APIKEY}")
+    suspend fun searchForCompanies(
+        @Query("page") page: Int,
+        @Query("query") query: String
+    )
+
+    @GET("search/tv?api_key=${BuildConfig.APIKEY}")
+    suspend fun searchForTvShows(
+        @Query("page") page: Int,
+        @Query("query") query: String,
+        @Query("include_adult") includeAdult: Boolean = true
+    )
+
+    @GET("search/person?api_key=${BuildConfig.APIKEY}")
+    suspend fun searchForPerson(
+        @Query("page") page: Int,
+        @Query("query") query: String,
+        @Query("include_adult") includeAdult: Boolean = true
+    )
+
+    @GET("search/movie?api_key=${BuildConfig.APIKEY}")
+    suspend fun searchForMovies(
+        @Query("page") page: Int,
+        @Query("query") query: String,
+        @Query("include_adult") includeAdult: Boolean = true
+    )
+
+    @GET("search/multi?api_key=${BuildConfig.APIKEY}")
+    suspend fun multiSearch(
+        @Query("page") page: Int,
+        @Query("query") query: String,
+        @Query("include_adult") includeAdult: Boolean = true
+    )
+
+    @GET("search/keyword?api_key=${BuildConfig.APIKEY}")
+    suspend fun keywordSearch(
+        @Query("page") page: Int,
+        @Query("query") query: String,
+    )
+
+    @GET("search/collection?api_key=${BuildConfig.APIKEY}")
+    suspend fun searchForCollections(
+        @Query("page") page: Int,
+        @Query("query") query: String,
+    )
 }
