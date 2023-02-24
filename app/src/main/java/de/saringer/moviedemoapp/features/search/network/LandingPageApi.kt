acb1adca0f3c2c33d928f1760497fdab9f7bcb31
Justus-Saringer <jsaringer@gmx.de>
2023-02-25 00:41:19 +0100

    added landing page api routes

--- app/src/main/java/de/saringer/moviedemoapp/features/search/network/LandingPageApi.kt
+++ app/src/main/java/de/saringer/moviedemoapp/features/search/network/LandingPageApi.kt
@@ -1,5 +1,34 @@
 package de.saringer.moviedemoapp.features.search.network
 
-interface LandingPageApi {
+import de.saringer.moviedemoapp.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface LandingPageApi {
+    // TODO: create Responses
+    @GET("discover/movie?sort_by=popularity.desc")
+    suspend fun getMostPopularMovies(
+        @Query("api_key") apiKey: String? = BuildConfig.APIKEY,
+        @Query("page") page: Int,
+    )
+
+    @GET("discover/movie/?certification_country=US&certification=R&sort_by=vote_average.descdiscover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc")
+    suspend fun getHighRatedMovies(
+        @Query("api_key") apiKey: String? = BuildConfig.APIKEY,
+        @Query("page") page: Int,
+    )
+
+    @GET("discover/movie?sort_by=vote_average.desc")
+    suspend fun getBestMoviesFromYear(
+        @Query("api_key") apiKey: String? = BuildConfig.APIKEY,
+        @Query("primary_release_year") releaseYear: Int,
+        @Query("page") page: Int,
+    )
+
+    @GET("discover/movie?with_genres=878&sort_by=vote_average.desc")
+    suspend fun getHighestRatedScienceFictionMovies(
+        @Query("api_key") apiKey: String? = BuildConfig.APIKEY,
+        @Query("page") page: Int,
+    )
+//    https://developers.themoviedb.org/3/discover/movie-discover
 }
