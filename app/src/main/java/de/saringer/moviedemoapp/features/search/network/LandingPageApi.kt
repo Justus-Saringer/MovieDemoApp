a26c56baca3d9e3caee9aa23bc34ee0ff8773d0a
Justus-Saringer <jsaringer@gmx.de>
2023-02-25 13:39:51 +0100

    forgot to add this, it belongs to the last commit

--- app/src/main/java/de/saringer/moviedemoapp/features/search/network/LandingPageApi.kt
+++ app/src/main/java/de/saringer/moviedemoapp/features/search/network/LandingPageApi.kt
@@ -1,34 +1,34 @@
 package de.saringer.moviedemoapp.features.search.network
 
 import de.saringer.moviedemoapp.BuildConfig
+import de.saringer.moviedemoapp.features.search.network.model.DiscoverResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+//    https://developers.themoviedb.org/3/discover/movie-discover
 interface LandingPageApi {
-    // TODO: create Responses
     @GET("discover/movie?sort_by=popularity.desc")
     suspend fun getMostPopularMovies(
         @Query("api_key") apiKey: String? = BuildConfig.APIKEY,
         @Query("page") page: Int,
-    )
+    ): DiscoverResponse
 
     @GET("discover/movie/?certification_country=US&certification=R&sort_by=vote_average.descdiscover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc")
     suspend fun getHighRatedMovies(
         @Query("api_key") apiKey: String? = BuildConfig.APIKEY,
         @Query("page") page: Int,
-    )
+    ): DiscoverResponse
 
     @GET("discover/movie?sort_by=vote_average.desc")
     suspend fun getBestMoviesFromYear(
         @Query("api_key") apiKey: String? = BuildConfig.APIKEY,
         @Query("primary_release_year") releaseYear: Int,
         @Query("page") page: Int,
-    )
+    ): DiscoverResponse
 
     @GET("discover/movie?with_genres=878&sort_by=vote_average.desc")
     suspend fun getHighestRatedScienceFictionMovies(
         @Query("api_key") apiKey: String? = BuildConfig.APIKEY,
         @Query("page") page: Int,
-    )
-//    https://developers.themoviedb.org/3/discover/movie-discover
+    ): DiscoverResponse
 }
